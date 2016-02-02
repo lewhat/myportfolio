2fe2976026e144b813754d9575489bebf037ff6b
lewhat <lewhat@live.com>
2016-02-01 16:44:08 -0800

    more html and js updates

--- js/main.js
+++ js/main.js
@@ -192,3 +192,23 @@ $(window).on("load",function() {
   fade(); //Fade in completely visible elements during page-load
   $(window).scroll(function() {fade();}); //Fade in elements during scroll
 });
+
+
+$(window).on("load",function() {
+  function fade() {
+    $('.four').each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+      /* If the object is completely visible in the window, fade it in */
+      if (objectBottom < windowBottom) {
+        if ($(this).css('opacity')==0) {$(this).fadeTo(1000,1);}
+      } else {
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }
+  fade(); //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade();}); //Fade in elements during scroll
+});
