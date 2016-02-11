a2f7a58b82b7ca24b49a6ddebb931a3dd7f828bb
lewhat <lewhat@live.com>
2016-02-11 13:59:26 -0800

    more modification

--- js/main.js
+++ js/main.js
@@ -1,58 +1,46 @@
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
-
-/*jshint browser: true, strict: true, undef: true */
-/*global define: false */
 
 ( function( window ) {
 
-'use strict';
+'use strict'
 
 // class helper functions from bonzo https://github.com/ded/bonzo
 
 function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)")
 }
 
 // classList support for class management
 // altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
+var hasClass, addClass, removeClass
 
 if ( 'classList' in document.documentElement ) {
   hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
+    return elem.classList.contains( c )
+  }
   addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
+    elem.classList.add( c )
+  }
   removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
+    elem.classList.remove( c )
+  }
 }
 else {
   hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
+    return classReg( c ).test( elem.className )
+  }
   addClass = function( elem, c ) {
     if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
+      elem.className = elem.className + ' ' + c
     }
-  };
+  }
   removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
+    elem.className = elem.className.replace( classReg( c ), ' ' )
+  }
 }
 
 function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
+  var fn = hasClass( elem, c ) ? removeClass : addClass
+  fn( elem, c )
 }
 
 var classie = {
@@ -66,27 +54,27 @@ var classie = {
   add: addClass,
   remove: removeClass,
   toggle: toggleClass
-};
+}
 
 // transport
 if ( typeof define === 'function' && define.amd ) {
   // AMD
-  define( classie );
+  define( classie )
 } else {
   // browser global
-  window.classie = classie;
+  window.classie = classie
 }
 
-})( window );
+})( window )
 
 
 $(document).ready(function(){
 
     $('navbar-fixed-top').each(function(){
-     sections.push($($(this).attr('href')));
-    });   
+     sections.push($($(this).attr('href')))
+    })  
 
-});
+})
 
 
   
@@ -99,116 +87,127 @@ $(window).on("load",function() {
   function fade() {
     $('.storyText').each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top + $(this).outerHeight()
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight()
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1)}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0)}
       }
-    });
+    })
   }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
-});
+  fade() //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade()
+}) //Fade in elements during scroll
+})
 
 $(window).on("load",function() {
   function fade() {
     $('.myLife').each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top + $(this).outerHeight()
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight()
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1)}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0)}
       }
-    });
+    })
   }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
-});
+  fade() //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade()
+  }) //Fade in elements during scroll
+})
 
 
 $(window).on("load",function() {
   function fade() {
     $('.one').each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top + $(this).outerHeight()
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight()
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1)}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(1000,0);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(1000,0)}
       }
-    });
+    })
   }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
-});
+  fade() //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade()
+  }) //Fade in elements during scroll
+})
 
 
 $(window).on("load",function() {
   function fade() {
     $('.two').each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top + $(this).outerHeight()
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight()
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(750,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(750,1)}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(750,0);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(750,0)}
       }
-    });
+    })
   }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
-});
+  fade() //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade()
+  })//Fade in elements during scroll
+})
 
 
 $(window).on("load",function() {
   function fade() {
     $('.three').each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top + $(this).outerHeight()
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight()
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(1000,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(1000,1)}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0)}
       }
-    });
+    })
   }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
-});
+  fade() //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade()
+  }) //Fade in elements during scroll
+})
 
 
 $(window).on("load",function() {
   function fade() {
     $('.four').each(function() {
       /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      var objectBottom = $(this).offset().top + $(this).outerHeight()
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight()
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(1000,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(1000,1)}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0)}
       }
-    });
+    })
   }
-  fade(); //Fade in completely visible elements during page-load
-  $(window).scroll(function() {fade();}); //Fade in elements during scroll
-});
+  fade() //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade()
+  }) //Fade in elements during scroll
+})
+
+
+
+
+  
